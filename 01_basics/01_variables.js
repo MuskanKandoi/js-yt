736fd1f55126c78f36a414b2c8b66ff068e099c1
MuskanKandoi <130232180+MuskanKandoi@users.noreply.github.com>
2023-12-18 21:00:39 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553 //cant change
+let accountEmail= "muskan@gmail.com"
+var accountPassword = "12345"
+accountCity="bangalore"
+
+// accountId =2 cant change
+accountEmail="m@gmail.com"
+accountPassword="234"
+accountCity="jaipur"
+let accountState; //comes undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState] );
