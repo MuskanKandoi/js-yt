ae4d1da8043e4fbf54a471af557e9651f27298f7
MuskanKandoi <130232180+MuskanKandoi@users.noreply.github.com>
2023-12-19 17:17:12 +0000

    datatype

---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all code as newer version
+// mdn tc39
+// alert(3+3); //cant use as we are using node js not browser in browser you can use
+
+// number => 2 to the power 53
+// bigint
+//string=" "
+//boolean = true or false
+//null=standalone value
+// undefined
+//symbol=>unique
+
+
+//user defined
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
