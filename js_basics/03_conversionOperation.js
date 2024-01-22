300831572e7daca89c2c75c7fb67575d103dff73
MuskanKandoi <130232180+MuskanKandoi@users.noreply.github.com>
2024-01-22 18:49:35 +0000

    changes done

--- js_basics/03_conversionOperation.js
+++ js_basics/03_conversionOperation.js
@@ -38,3 +38,20 @@ let str1="hello"
 let str2=" muskan"
 let str3=str1+str2
 console.log(str3)
+
+console.log("1"+"2")
+console.log("1"+2)
+console.log("1"+2+2) //output= 122 string first them whole is considered as string
+console.log(1+2+"2") //output = 32 number first so it will be calculated
+console.log(1+"2")
+
+console.log(true) //output = true
+console.log(+true) //output= 1
+console.log(+"") //output=0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
